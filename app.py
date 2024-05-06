9e3a6ad8f026712bd827bbee2ab5c355221dbb3c
jnlee <jyl9604@nyu.edu>
2024-05-06 12:15:24 -0400

    modified box

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 import mysql.connector
 
 app = Flask(__name__)
-#put queries in a dictionary, keys are 1-5.
 @app.route('/')
 def home():
     questions = {1:"Revenue Change From This Year To Last",
@@ -17,6 +16,19 @@ def category():
     #the query will return number 1-5 coinciding with which prompt
     #create a dictionary structure with key 1-5, and the actual queries, then run the query and get back
     #the tuple
+    dict = {
+        1:"Query1",
+        2:"Query2",
+        3:"Query3",
+        4:"Query4",
+        5:"Query5"
+     }
+    query = dict[int(queryCase)]
+    #cursor.execute(query)
+    #tuple = cursor.fetchall()
+    #after getting info, put into dictionary form.
+    #ex:(rev this year:#,rev last year:#),(visitors this year:#, visitors last year:#),
+    # (customer spending:#),(season:visitors),(Number1:countery,Number2:country,Number3:country)
     ret = {
         "Number of Customers":230,
         "Number of Repeat Customers":50
@@ -24,7 +36,7 @@ def category():
     other = {
         "test 2":100
     }
-    if int(queryCase) < 3:
+    if int(queryCase) == 3:
         return render_template('category.html',values=ret)
     else:
         return render_template('category.html',values=other)
